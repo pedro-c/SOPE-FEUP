5806483d065c86b542c3ae312f0d5f1aa894b858
Pedro Costa <pedrodc@outlook.com>
2016-03-05 18:14:52 +0000

    p24 completed

--- p2/p24.c
+++ p2/p24.c
@@ -16,13 +16,13 @@ int main(int argc, char *argv[])
    unsigned char buffer[BUFFER_SIZE]; 
    int file1, nr, nw;
    
-   if(argc != 1){
+   if(argc != 2){
        printf("Invalid number of arguments.\n");
        return 1;
    }
    
    int n=0;
-   write(STDOUT_FILENO, "How many students will you add? ", 42);
+   write(STDOUT_FILENO, "How many students will you add? ", 32);
    read(STDIN_FILENO, buffer, BUFFER_SIZE);
    n=atoi(buffer);
    
@@ -32,30 +32,30 @@ int main(int argc, char *argv[])
         close(file1);
         return 2;
     }
+    
+    dup2(file1,STDOUT_FILENO);
    
    int i=0;
    for(i; i<n; i++){
        
+       printf("Name:");
         write(STDOUT_FILENO, "Name:", 5);
         nr=read(STDIN_FILENO, buffer, BUFFER_SIZE);
-        if((nw = fwrite(buffer, 1, BUFFER_SIZE, file1)) <= 0 || nw != nr){
+        if((nw = write(STDOUT_FILENO, buffer, nr)) <= 0 || nw != nr){
             close(file1);
             return 3;
         }
         
+        printf("Score:");
         write(STDOUT_FILENO, "Score:", 6);
         nr=read(STDIN_FILENO, buffer, BUFFER_SIZE);
-        if((nw = fwrite(buffer, 1, BUFFER_SIZE, file1)) <= 0 || nw != nr){
+        if((nw = write(STDOUT_FILENO, buffer, nr)) <= 0 || nw != nr){
             close(file1);
-            return 3;
+            return 4;
         }
         
              
    }
-   
-   
-   
-   
-   
-    return 0;
+  
+   return 0;
 } 
